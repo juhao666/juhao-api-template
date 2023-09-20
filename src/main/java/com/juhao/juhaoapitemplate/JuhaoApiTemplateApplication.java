10e0a312e655f53a34dbf2594f7346a6fed4f6e0
Erik Liu <liu_jianying1985@hotmail.com>
2023-09-20 08:55:20 +0800

    first commit

---
+++ src/main/java/com/juhao/juhaoapitemplate/JuhaoApiTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.juhao.juhaoapitemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuhaoApiTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuhaoApiTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juhao/juhaoapitemplate/JuhaoApiTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juhao.juhaoapitemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuhaoApiTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
